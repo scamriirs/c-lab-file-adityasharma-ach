3aeb67bc3f0b3e5bae314562ce745a8166960f44
Aditya_Sharma <137752484+adityasharma-ach@users.noreply.github.com>
2025-02-27 11:45:47 +0530

    Create Program 11.cs

---
+++ Program 11.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Student
+{
+    public string Name;
+    public int Age;
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student();
+        student1.Name = "Aditya";
+        student1.Age = 21;
+        student1.Display();
+    }
+}
